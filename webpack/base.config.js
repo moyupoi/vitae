bb032d17b6ca8103394e9f3b450f28daccc8fe4e
tuoxie15 <tuoxie18@gmail.com>
2017-07-25 13:40:45 +0800

    FIX

---
+++ webpack/base.config.js
@@ -0,0 +1,66 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const extpath = path.join(__dirname, '../src/browser/extension/');
+const mock = `${extpath}chromeAPIMock.js`;
+
+const baseConfig = (params) => ({
+  entry: params.input || {
+    background: [ mock, `${extpath}background/index` ],
+    options: [ mock, `${extpath}options/index` ],
+    window: [ `${extpath}window/index` ],
+    remote: [ `${extpath}window/remote` ],
+    devpanel: [ mock, `${extpath}devpanel/index` ],
+    devtools: [ `${extpath}devtools/index` ],
+    content: [ mock, `${extpath}inject/contentScript` ],
+    pagewrap: [ `${extpath}inject/pageScriptWrap` ],
+    'redux-devtools-extension': [ `${extpath}inject/index`, `${extpath}inject/deprecatedWarn` ],
+    inject: [ `${extpath}inject/index`, `${extpath}inject/deprecatedWarn` ],
+    ...params.inputExtra
+  },
+  output: {
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].chunk.js',
+    ...params.output
+  },
+  plugins: [
+    new webpack.DefinePlugin(params.globals),
+    ...(params.plugins ? params.plugins :
+      [
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+          comments: false,
+          compressor: {
+            warnings: false
+          },
+          mangle: {
+            screw_ie8: true,
+            keep_fnames: true
+          }
+        })
+      ])
+  ],
+  resolve: {
+    alias: {
+      app: path.join(__dirname, '../src/app'),
+      tmp: path.join(__dirname, '../build/tmp')
+    },
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      ...(params.loaders ? params.loaders : [{
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /(node_modules|tmp\/page\.bundle)/
+      }]),
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'raw-loader'],
+      }
+    ]
+  }
+});
+
+export default baseConfig;
